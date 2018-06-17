a2545e6acde06eece03a3bf223b3ec5def4e6464
sunyuejun <biiqnypl@163.com>
2018-06-17 10:47:29 +0800

    添加匿名方法示例

---
+++ Excerse/匿名方法/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 匿名方法
+{
+    delegate void SayGeeting(string m);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayGeeting sg = new SayGeeting(SayChinese);
+            sg("Jack");
+
+            SayGeeting sg1 = m => Console.WriteLine($"你好，{m} 帅哥！");
+            sg1("John");
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static void SayChinese(string m)
+        {
+            Console.WriteLine($"你好，{m}");
+        }
+    }
+}
